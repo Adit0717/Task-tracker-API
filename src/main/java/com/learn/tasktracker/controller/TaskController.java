ea7dcac2801ff4964e92c53abffef9a34faca542
Aditya Krishnamurthi <krisa04@pfw.edu>
2026-06-02 01:03:18 -0400

    Fixed Error 591

--- src/main/java/com/learn/tasktracker/controller/TaskController.java
+++ src/main/java/com/learn/tasktracker/controller/TaskController.java
@@ -30,9 +30,7 @@ public class TaskController {
     public ResponseEntity<List<Task>> getAllTasks(
             @RequestParam(required = false) TaskStatus status,
             @RequestParam(required = false) Integer priority) {
-        if (status != null || priority != null) {
-            return ResponseEntity.ok(taskService.getAllTasks(null, null));
-        }
+
         return ResponseEntity.ok(taskService.getAllTasks(status, priority));
     }
 
