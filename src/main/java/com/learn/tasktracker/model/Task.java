6a8bf21c4535ca2daee69d270e74a8fe5ecb6f0d
Aditya Krishnamurthi <krisa04@pfw.edu>
2026-06-02 01:25:29 -0400

    Fixed Drill 347

--- src/main/java/com/learn/tasktracker/model/Task.java
+++ src/main/java/com/learn/tasktracker/model/Task.java
@@ -13,7 +13,7 @@ public class Task {
     private Long id;
 
     @Column(nullable = false)
-    @com.fasterxml.jackson.annotation.JsonIgnore
+    // @com.fasterxml.jackson.annotation.JsonIgnore
     private String title;
 
     private String description;
